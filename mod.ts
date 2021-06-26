e041dbb15c06149c1cb624871bcf2a92e93accd7
Ben Moon <software@guiltydolphin.com>
2021-06-26 18:59:35 +0100

    Add exports for remaining modules to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,9 @@
 export * as array from './src/array.ts';
 
+export * as fun from './src/function.ts';
+
+export * as functor from './src/functor.ts';
+
 import * as functional from './src/functional.ts';
 
 export import either = functional.either;
@@ -8,4 +12,10 @@ export import Either = functional.Either;
 export import maybe = functional.maybe;
 export import Maybe = functional.Maybe;
 
+export * as generic from './src/generic.ts';
+
+export * as monad from './src/monad.ts';
+
 export * as types from './src/types.ts';
+
+export * as unwrap from './src/unwrap.ts';
--- tests/exports/exports_mod_test.ts
+++ tests/exports/exports_mod_test.ts
@@ -8,9 +8,14 @@ import {
     array,
     either,
     Either,
+    fun,
+    functor,
+    generic,
     maybe,
     Maybe,
+    monad,
     types,
+    unwrap,
 } from '../../mod.ts';
 
 testGroup('mod.ts',
@@ -19,8 +24,18 @@ testGroup('mod.ts',
     new Test('exports Either type', () => assertExists(true as unknown as Either<Number, Boolean>)),
     new Test('exports either namespace', () => assertExists(either)),
 
+    new Test('exports fun namespace', () => assertExists(fun)),
+
+    new Test('exports functor namespace', () => assertExists(functor)),
+
+    new Test('exports generic namespace', () => assertExists(generic)),
+
     new Test('exports Maybe type', () => assertExists(true as unknown as Maybe<Number>)),
     new Test('exports maybe namespace', () => assertExists(maybe)),
 
+    new Test('exports monad namespace', () => assertExists(monad)),
+
     new Test('exports types namespace', () => assertExists(types)),
+
+    new Test('exports unwrap namespace', () => assertExists(unwrap)),
 ).runAsMain();
